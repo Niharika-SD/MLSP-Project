87f829284f7de7c6aaa753971710cb973fa42f5b
ShimonaNiharika <Shimona.Niharika.Dsouza@gmail.com>
2016-11-09 22:29:30 -0500

    timecourse extraction

---
+++ TC_extract.m
@@ -0,0 +1,24 @@
+ aal  = load_nii('aal_MNI_V4.nii');
+ 
+ datafolder = ('/mnt/share/7E1EC9C11EC972A7/fmridata');
+ 
+ filename = dir([datafolder '/*/*/Functionals/*.nii.gz']);
+ 
+ patient =[];
+ 
+ for k =1:size(filename,1)
+     
+     nii = load_nii(filename(k).name);
+     patient(k).fmridata(:,:,:,:) = nii.img;
+     n = size(nii.img,4);
+     for j = 1:max(max(max(aal.img)))
+         mask = (aal.img ==j);
+         mask(:,:,:,1:n) = 1;
+         ROI = mask.*patient(k).fmridata(:,:,:,:);
+         patient(k).TC(:,j) = avg_TC(ROI,mask);
+     end
+ end
+ 
+ save patient
+ 
+ 
